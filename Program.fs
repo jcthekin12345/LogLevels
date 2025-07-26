ba006c483380653bd34b2f2a1c666e2a6b47121f
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-26 06:37:55 +0200

    docs: add comment explanations for function break down

--- Program.fs
+++ Program.fs
@@ -1,13 +1,17 @@
 ﻿module LogLevels
 
-let message (logLine: string): string = 
-    let parts = logLine.Split(':')
-    if parts.Length > 1 then
-        parts.[1].Trim()
-    else
-        logLine.Trim()
-        
+open System
 
+
+let message (logLine: string): string =
+    let parts = logLine.Split(':') // binds an array of logLevel and message
+    if parts.Length > 1 then // checks if the array has at least 2 elements 
+        parts.[1].Trim() // trims the 2nd element which is the message
+    else
+        logLine.Trim() // else just trim the logline
+        
+        
+    
 let logLevel(logLine: string): string = failwith "[WARNING]: Disk almost full\r\n"
 
 let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
