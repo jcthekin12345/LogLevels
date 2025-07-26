d7217e657539affcd605e3b3a208908ad4491082
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-26 06:39:23 +0200

    refactor: removed message function body

--- Program.fs
+++ Program.fs
@@ -4,12 +4,7 @@ open System
 
 
 let message (logLine: string): string =
-    let parts = logLine.Split(':') // binds an array of logLevel and message
-    if parts.Length > 1 then // checks if the array has at least 2 elements 
-        parts.[1].Trim() // trims the 2nd element which is the message
-    else
-        logLine.Trim() // else just trim the logline
-        
+    
         
     
 let logLevel(logLine: string): string = failwith "[WARNING]: Disk almost full\r\n"
