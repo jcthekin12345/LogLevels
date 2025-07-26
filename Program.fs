b7833891aa2716909f356c3fa3ba3f54bff8ccaf
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-26 06:09:01 +0200

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿module LogLevels
+
+let message (logLine: string): string = 
+    logLine.Split(' ', '\t').ToString()
+
+let logLevel(logLine: string): string = failwith "[WARNING]: Disk almost full\r\n"
+
+let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+
+let myMessage = message "[WARNING]: Disk Almost Full"
+printfn "%s" myMessage
